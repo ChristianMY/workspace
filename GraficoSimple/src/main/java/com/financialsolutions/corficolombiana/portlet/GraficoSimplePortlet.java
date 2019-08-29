2bc10f6b190df239e0673903a690f3bdc5405d71
Christian Morales <eljack390@gmail.com>
2019-08-29 11:41:24 -0500

    Create GraficoSimplePortlet.java

---
+++ GraficoSimple/src/main/java/com/financialsolutions/corficolombiana/portlet/GraficoSimplePortlet.java
@@ -0,0 +1,28 @@
+package com.financialsolutions.corficolombiana.portlet;
+
+import com.financialsolutions.corficolombiana.constants.GraficoSimplePortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author iramirez
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + GraficoSimplePortletKeys.GraficoSimple,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class GraficoSimplePortlet extends MVCPortlet {
+}
