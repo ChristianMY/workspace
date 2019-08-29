92008ec15993c77bbce60d9698387208cd0e2438
Christian Morales <eljack390@gmail.com>
2019-08-29 11:41:14 -0500

    Create GraficoSimplePortletKeys.java

---
+++ GraficoSimple/src/main/java/com/financialsolutions/corficolombiana/constants/GraficoSimplePortletKeys.java
@@ -0,0 +1,10 @@
+package com.financialsolutions.corficolombiana.constants;
+
+/**
+ * @author iramirez
+ */
+public class GraficoSimplePortletKeys {
+
+	public static final String GraficoSimple = "graficosimple";
+
+}
